50664c486b44775eb9a7c45078f8bf16ca31c80e
jiwon <usagi0012@naver.com>
2023-11-14 17:42:30 +0900

    할일 순서 변경 API 구현

--- routes/todos.router.js
+++ routes/todos.router.js
@@ -26,4 +26,31 @@ router.get("/todos", async (req, res) => {
   res.send({ todos });
 });
 
+//할일 목록 순서 변경 API
+router.patch("/todos/:todoId", async (req, res) => {
+  const { todoId } = req.params;
+  const { order } = req.body;
+
+  //todoID에 해당하는 할일이 있는지 확인 후 없으면 에러 출력
+  const currentTodo = await Todo.findById(todoId);
+  if (!currentTodo) {
+    throw new Error("존재하지 않는 todo 데이터입니다.");
+  }
+
+  //order값이 있을 때만 순서 변경
+  if (order) {
+    const targetTodo = await Todo.findOne({ order }).exec();
+    if (targetTodo) {
+      //대상과 바꿀 목표의 오더값을 변경
+      targetTodo.order = currentTodo.order;
+      await targetTodo.save();
+    }
+    //대상의 오더값을 변경
+    currentTodo.order = order;
+  }
+  await currentTodo.save();
+
+  res.send({});
+});
+
 module.exports = router;
