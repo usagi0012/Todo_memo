22e5c956514cec6d0a265e06a8046c015bd72885
jiwon <usagi0012@naver.com>
2023-11-14 17:32:19 +0900

    할일 목록 조회 API 생성

--- routes/todos.router.js
+++ routes/todos.router.js
@@ -7,6 +7,7 @@ router.get("/", (req, res) => {
   res.send("Hi!");
 });
 
+//할일 생성 API
 router.post("/todos", async (req, res) => {
   const { value } = req.body;
   const maxOrderByUserId = await Todo.findOne().sort("-order").exec();
@@ -16,6 +17,13 @@ router.post("/todos", async (req, res) => {
   const todo = new Todo({ value, order });
   await todo.save();
   res.send({ todo });
+});
+
+//할일 목록 조회 API
+router.get("/todos", async (req, res) => {
+  const todos = await Todo.find().sort("-order").exec();
+
+  res.send({ todos });
 });
 
 module.exports = router;
